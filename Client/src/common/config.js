5e118e2f7a35304e110a18e9a4381959da53ae87
abhaykoli1 <abhaykoli214@gmail.com>
2026-03-13 23:29:22 +0530

    dasda

---
+++ Client/src/common/config.js
@@ -0,0 +1,15 @@
+const config = {
+  Editor_API: "la9sqj5v9x0r7zlmfho78qpvz8x8kbcjplfuhrwtbplsfvvo",
+  //:local host
+  // API_URL: "http://localhost:8080",
+
+  // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // live host
+  API_URL: "https://api.dnsdxb.com",
+  
+  // API_URL: "https://realestatebackend-vjq3.onrender.com",
+
+  // API_URL: "https://real-esate-oghre.ondigitalocean.app",
+};
+
+export default config;
